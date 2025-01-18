6b3b79b2af220c9bb042bc3944fc0fb64e3d4da5
Vojtech Ruzicka <vojtech.ruz@gmail.com>
2025-01-18 11:58:07 +0100

    Basic application structure

---
+++ src/main/java/com/vojtechruzicka/xjsexporter/XjsExporterApplication.java
@@ -0,0 +1,13 @@
+package com.vojtechruzicka.xjsexporter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XjsExporterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XjsExporterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vojtechruzicka/xjsexporter/XjsExporterTest.java
@@ -0,0 +1,13 @@
+package com.vojtechruzicka.xjsexporter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoxjsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
